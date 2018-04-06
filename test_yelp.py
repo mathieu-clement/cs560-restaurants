a56d48cae2f9a2d29ed0321a6c685668d0ff1cfe
Mathieu Clement <mclement2@dons.usfca.edu>
2018-04-06 09:29:55 -0700

    improve coverage

--- test_yelp.py
+++ test_yelp.py
@@ -6,14 +6,18 @@ from yelp import YelpClient
 class TestYelpClient(unittest.TestCase):
 
     def test_ks_kitchen(self):
-        data = YelpClient().search_business("K's kitchen", '757 Monterey Blvd', '94127')
+        data = YelpClient().search_business("K's Kitchen", '757 Monterey Blvd', '94127')
 
+        self.assertEqual(data['name'], "K's Kitchen")
         self.assertEqual(data['phone'], '+14153338500')
         self.assertEqual(data['location']['address1'], '757 Monterey Blvd')
+        self.assertEqual(data['location']['zip_code'], '94127')
+        self.assertEqual(data['price'], '$$')
+        self.assertTrue(data['rating'] >= 1.0 and data['rating'] <= 5.0)
 
         categories = list(map(lambda c: c['title'], data['categories']))
         self.assertTrue('Japanese' in categories)
         self.assertTrue('Sushi Bars' in categories)
 
-if __name__ == '__main__':
-    unittest.main()
+#if __name__ == '__main__':
+#    unittest.main()
