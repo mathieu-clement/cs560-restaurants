bf5707bbe10c53f3761c11927939c7122d08f209
Mathieu Clement <mclement2@dons.usfca.edu>
2018-04-10 12:37:45 -0700

    WIP

--- fetch.py
+++ fetch.py
@@ -6,17 +6,31 @@ from yelp import YelpClient
 class ScoreYelpFetcher:
 
     def __init__(self, scores_filename):
-        self.scores_reader = ScoresReader(filename)
+        self.scores_reader = ScoresReader(scores_filename)
         self.yelp_client = YelpClient()
 
     def fetch(self, start_score_id, num_restaurants):
         """Reads num_restaurants restaurants from the inspection dataset
         starting from restaurant with ID start_score_id."""
 
+        results = {}
+
         ids = self.scores_reader.get_ids(start_score_id, num_restaurants)
+        print('Ids', ids)
 
         for business_id in ids:
+            row = self.scores_reader.get_row(business_id)
             data = self.yelp_client.search_business(
-                    row['business_name'],
-                    row['business_address'],
-                    row['business_postal_code'])
+                    row['name'],
+                    row['address'],
+                    row['zip_code'])
+            results[business_id] = data
+
+        return results
+
+
+if __name__ == '__main__':
+    fetcher = ScoreYelpFetcher('restaurant_scores.csv')
+    from pprint import pprint
+    pprint(fetcher.fetch(0, 2))
+
